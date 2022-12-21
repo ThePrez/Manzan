eb074070c6e7404919b63b34ee0be02085231162
Liam Barry Allan <mrliamallan@live.co.uk>
2022-12-21 13:24:13 -0500

    Pass in creds to twilio constructor
    Signed-off-by: Liam Barry Allan <mrliamallan@live.co.uk>

--- camel/src/main/java/com/github/theprez/manzan/routes/dest/TwilioDestination.java
+++ camel/src/main/java/com/github/theprez/manzan/routes/dest/TwilioDestination.java
@@ -9,12 +9,12 @@ import org.apache.camel.component.twilio.TwilioComponent;
 import com.github.theprez.manzan.routes.ManzanGenericCamelRoute;
 
 public class TwilioDestination extends ManzanGenericCamelRoute {
-    public TwilioDestination(CamelContext context, final String _name, final String _format, final Map<String, String> _uriParams) {
+    public TwilioDestination(CamelContext context, final String _name, final String _format, String sid, String token, final Map<String, String> _uriParams) {
         super(_name, "twilio", "message/create", _format, _uriParams, null);
 
         TwilioComponent twilio = context.getComponent("twilio", TwilioComponent.class);
-        twilio.setUsername(m_uriParams.remove("sid").toString());
-        twilio.setPassword(m_uriParams.remove("token").toString());
+        twilio.setUsername(sid);
+        twilio.setPassword(token);
     }
 
     @Override
