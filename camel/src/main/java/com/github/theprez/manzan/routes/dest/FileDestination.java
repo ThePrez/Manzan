310205a87c2c286a06322730eac0dee4e03564f4
Jesse Gorzinski <17914061+ThePrez@users.noreply.github.com>
2023-12-30 18:04:42 -0600

    Fix `file` destination to use Camel Stream endpoint

--- camel/src/main/java/com/github/theprez/manzan/routes/dest/FileDestination.java
+++ camel/src/main/java/com/github/theprez/manzan/routes/dest/FileDestination.java
@@ -1,5 +1,6 @@
 package com.github.theprez.manzan.routes.dest;
 
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 import org.apache.camel.Exchange;
@@ -8,7 +9,13 @@ import com.github.theprez.manzan.routes.ManzanGenericCamelRoute;
 
 public class FileDestination extends ManzanGenericCamelRoute {
     public FileDestination(final String _name, final String _file, final String _format, final Map<String, String> _uriParams) {
-        super(_name, "file", _file, _format, _uriParams, null);
+        super(_name, "stream", "file", _format, addToMap(_uriParams, "fileName", _file), null);
+    }
+
+    private static Map<String, String> addToMap(Map<String, String> _uriParams, String _key, String _val) {
+        Map<String, String> ret = null == _uriParams ? new LinkedHashMap<String,String>(): _uriParams;
+        ret.put(_key, _val);
+        return ret;
     }
 
     @Override
