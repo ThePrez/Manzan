dea7b265b84c3f0f3287e449b7ffa57ebfa28372
Jonathan Zak <Jonathan.Zak@ibm.com>
2024-07-18 11:10:24 -0400

    Use secure email protocol

--- camel/src/main/java/com/github/theprez/manzan/routes/dest/EmailDestination.java
+++ camel/src/main/java/com/github/theprez/manzan/routes/dest/EmailDestination.java
@@ -8,7 +8,7 @@ import com.github.theprez.manzan.routes.ManzanGenericCamelRoute;
 public class EmailDestination extends ManzanGenericCamelRoute {
 
     public EmailDestination(final String _name, final String _smtpServer, final String _format, final Map<String, String> _uriParams, final Map<String, Object> _headerParams) {
-        super(_name, "smtp", _smtpServer, _format, _uriParams, _headerParams);
+        super(_name, "smtps", _smtpServer, _format, _uriParams, _headerParams);
     }
 
     @Override
