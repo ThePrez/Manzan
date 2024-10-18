65e6682ff268724fdaa4873ebf83086cdbfddd17
Jonathan Zak <jonnyzak32@gmail.com>
2024-10-18 09:40:12 -0400

    format

--- camel/src/main/java/com/github/theprez/manzan/routes/event/WatchMsgEvent.java
+++ camel/src/main/java/com/github/theprez/manzan/routes/event/WatchMsgEvent.java
@@ -33,62 +33,64 @@ public class WatchMsgEvent extends ManzanRoute {
     @Override
     public void configure() {
         from("timer://foo?synchronous=true&period=" + m_interval)
-        .routeId("manzan_msg:"+m_name)
-        .process(exchange -> {
-            // Reset the list of ordinal positions at the start of each execution
-            exchange.setProperty("ordinalPositions", new ArrayList<Integer>());
-        })
-        .setHeader(EVENT_TYPE, constant(ManzanEventType.WATCH_MSG))
-        .setBody(constant("SeLeCt * fRoM " + m_schema + ".mAnZaNmSg wHeRe SESSION_ID = '"+m_sessionId+"' limit " + m_numToProcess ))
-        .to("jdbc:jt400?outputType=StreamList")
-        .split(body()).streaming().parallelProcessing()
-            .setHeader("id", simple("${body[ORDINAL_POSITION]}"))
-            .setHeader("session_id", simple("${body[SESSION_ID]}"))
-            .setHeader("data_map", simple("${body}"))
-            .process(exchange -> {
-                Integer ordinalPosition = exchange.getIn().getHeader("id", Integer.class);
-                @SuppressWarnings("unchecked")
-                List<Integer> ordinalPositions = exchange.getProperty("ordinalPositions", List.class);
+                .routeId("manzan_msg:" + m_name)
+                .process(exchange -> {
+                    // Reset the list of ordinal positions at the start of each execution
+                    exchange.setProperty("ordinalPositions", new ArrayList<Integer>());
+                })
+                .setHeader(EVENT_TYPE, constant(ManzanEventType.WATCH_MSG))
+                .setBody(constant("SeLeCt * fRoM " + m_schema + ".mAnZaNmSg wHeRe SESSION_ID = '" + m_sessionId
+                        + "' limit " + m_numToProcess))
+                .to("jdbc:jt400?outputType=StreamList")
+                .split(body()).streaming().parallelProcessing()
+                .setHeader("id", simple("${body[ORDINAL_POSITION]}"))
+                .setHeader("session_id", simple("${body[SESSION_ID]}"))
+                .setHeader("data_map", simple("${body}"))
+                .process(exchange -> {
+                    Integer ordinalPosition = exchange.getIn().getHeader("id", Integer.class);
+                    @SuppressWarnings("unchecked")
+                    List<Integer> ordinalPositions = exchange.getProperty("ordinalPositions", List.class);
 
-                synchronized (ordinalPositions) {
-                    ordinalPositions.add(ordinalPosition);  // Accumulate ORDINAL_POSITION
-                }
-            })
-            .marshal().json(true) //TODO: skip this if we are applying a format
-            .setBody(simple("${body}\n"))
-            .process(exchange -> {
-                if (null != m_formatter) {
-                    exchange.getIn().setBody(m_formatter.format(getDataMap(exchange)));
-                }
-            })
-            .recipientList(constant(getRecipientList()))
-            .parallelProcessing()
-            .stopOnException()
-            .end()
-        .end()
-        .process(exchange -> {
-            // Constructing the WHERE clause for ORDINAL_POSITIONs
-            StringBuilder deleteQuery = new StringBuilder("DELETE FROM " + m_schema + ".mAnZaNmSg WHERE SESSION_ID = '" + m_sessionId + "' AND ORDINAL_POSITION IN (");
-            @SuppressWarnings("unchecked")
-            List<Integer> ordinalPositions = exchange.getProperty("ordinalPositions", List.class);
-            if (ordinalPositions != null && !ordinalPositions.isEmpty()) {
-                synchronized (ordinalPositions) {
-                    String positions = ordinalPositions.stream()
-                        .map(String::valueOf)   // Convert to String
-                        .collect(Collectors.joining(","));
-                    deleteQuery.append(positions).append(")");
-                }
-                
-                // Set the DELETE query as the body
-                exchange.getIn().setBody(deleteQuery.toString());
-            } else {
-                // If no positions, set body to null, skip the DELETE
-                exchange.getIn().setBody(null);
-            }
-        })
-        .choice()
-            .when(body().isNotNull())
-            .to("jdbc:jt400")
-            .to("stream:err");
+                    synchronized (ordinalPositions) {
+                        ordinalPositions.add(ordinalPosition); // Accumulate ORDINAL_POSITION
+                    }
+                })
+                .marshal().json(true) // TODO: skip this if we are applying a format
+                .setBody(simple("${body}\n"))
+                .process(exchange -> {
+                    if (null != m_formatter) {
+                        exchange.getIn().setBody(m_formatter.format(getDataMap(exchange)));
+                    }
+                })
+                .recipientList(constant(getRecipientList()))
+                .parallelProcessing()
+                .stopOnException()
+                .end()
+                .end()
+                .process(exchange -> {
+                    // Constructing the WHERE clause for ORDINAL_POSITIONs
+                    StringBuilder deleteQuery = new StringBuilder("DELETE FROM " + m_schema
+                            + ".mAnZaNmSg WHERE SESSION_ID = '" + m_sessionId + "' AND ORDINAL_POSITION IN (");
+                    @SuppressWarnings("unchecked")
+                    List<Integer> ordinalPositions = exchange.getProperty("ordinalPositions", List.class);
+                    if (ordinalPositions != null && !ordinalPositions.isEmpty()) {
+                        synchronized (ordinalPositions) {
+                            String positions = ordinalPositions.stream()
+                                    .map(String::valueOf) // Convert to String
+                                    .collect(Collectors.joining(","));
+                            deleteQuery.append(positions).append(")");
+                        }
+
+                        // Set the DELETE query as the body
+                        exchange.getIn().setBody(deleteQuery.toString());
+                    } else {
+                        // If no positions, set body to null, skip the DELETE
+                        exchange.getIn().setBody(null);
+                    }
+                })
+                .choice()
+                .when(body().isNotNull())
+                .to("jdbc:jt400")
+                .to("stream:err");
     }
 }
