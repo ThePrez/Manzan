96bf803d055393684ead3fa995089611b9abdb98
Jonathan Zak <jonnyzak32@gmail.com>
2025-08-07 08:24:51 -0400

    remove header params in sql test

--- camel/src/test/java/CamelTests/SqlEventTest.java
+++ camel/src/test/java/CamelTests/SqlEventTest.java
@@ -30,15 +30,11 @@ public class SqlEventTest extends CamelTestHelper {
         final String sqlEvent = "sqlEvent";
         final String sql = "SELECT JOB_NAME, JOB_USER, SUBSYSTEM, JOB_STATUS, CPU_TIME, ELAPSED_CPU_PERCENTAGE, TOTAL_DISK_IO_COUNT, ELAPSED_TIME, TEMPORARY_STORAGE, MEMORY_POOL, FUNCTION, THREAD_COUNT FROM TABLE(QSYS2.ACTIVE_JOB_INFO()) AS X WHERE ELAPSED_CPU_PERCENTAGE > 20 OR TOTAL_DISK_IO_COUNT > 100000 ORDER BY ELAPSED_CPU_PERCENTAGE DESC FETCH FIRST 20 ROWS ONLY";
 
-        final String format = null;
-        final Map<String, String> headerParams = new LinkedHashMap();
-        headerParams.put("authorization", "bearer eyt");
-
         dataMapInjections.put("FOO", "BAR");
         destinations.add(testOutDest);
 
         return new RoutesBuilder[]{
-                new WatchSql(sqlEvent, sql, format, destinations, pollInterval, dataMapInjections),
+                new WatchSql(sqlEvent, sql, null, destinations, pollInterval, dataMapInjections),
                 new StreamDestination(context, testOutDest, null, componentOptions)
         };
     }
