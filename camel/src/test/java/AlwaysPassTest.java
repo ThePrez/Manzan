a9fb4bee7732258e33b7f552b0ed402377d5dd4c
Hisham Siddique <hisham.siddique@dal.ca>
2026-01-07 11:00:59 -0500

    Create AlwaysPassTest.java
    Validation check

---
+++ camel/src/test/java/AlwaysPassTest.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AlwaysPassTest {
+    @Test
+    void testAlwaysPasses() {
+        assertTrue(true);
+    }
+}
